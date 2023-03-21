ccc0134647c14befdee669e1e3d6c9fab37d9f26
Alison Park <alisonpark2023@u.northwestern.edu>
2023-03-21 00:06:00 -0400

    cute modal edits

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -9,14 +9,14 @@ const Modal = props => {
         <div className="modal" onClick={props.onClose}>
             <div className="modal-content" onClick={e => e.stopPropagation()}>
                 <div className='modal-header'>
-                <h4 className="modal-title"> Modal title</h4>
+                <h4 className="modal-title"> Hello!</h4>
                 </div>
            
             <div className="modal-body">
-                This is modal content
+                I'm Aly, a computer scientist at Northwestern University.
             </div>
             <div className="modal-footer">
-                <button onClick={props.onClose} className="button">Close</button>
+                <button onClick={props.onClose} className="button">❌</button>
             </div>
         </div>
         </div>
