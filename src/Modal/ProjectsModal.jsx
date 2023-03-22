6735167fa768d658d959e13a2b61c4d9f77fc4e1
Alison Park <alisonpark2023@u.northwestern.edu>
2023-03-21 21:59:51 -0400

    empty resume link

--- src/Modal/ProjectsModal.jsx
+++ src/Modal/ProjectsModal.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, {Linking} from 'react'
 import './Modal.css'
 
+function addSpace(str) {
+    return str.split('').join(' ');
+  }
+const word=addSpace("y ");
 const ProjectsModal = props => {
     if(!props.showProject){
         return null
@@ -15,10 +19,14 @@ const ProjectsModal = props => {
             <div className="modal-body">
                 <center>
                 <p></p>
-               This page is currently being updated, but if you'd like to learn more about my past projects feel free to check out my attached resume.
+               This page is currently being updated, but if you'd like to learn more about my past projects feel free to check out m{word}  
+               
+               <a className="resumeLink" href='/api/v1/print/example.pdf' target='_blank' rel='noopener noreferrer'>
+                     resume</a>
                <p></p>
-               <img src='../public/resume.png' width='100px'  height='100px' ></img>
-              
+                <a href='/api/v1/print/example.pdf' target='_blank' rel='noopener noreferrer'>
+                    <img src='../public/resume.png' width='100px'  height='100px' ></img>    
+               </a>
                 </center> 
                </div>
             <div className="modal-footer">
